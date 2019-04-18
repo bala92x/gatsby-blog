69c99033be6f789a5b595ec90d2286c1c7e17eec
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-18 12:21:35 +0200

    Add header and footer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 const AboutPage = () => {
     return (
         <div>
+            <Header />
             <h1>About Me</h1>
             <p>I currently work as a full-stack developer.</p>
             <p><Link to="/contact">Want to work with me? Reach out.</Link></p>
+            <Footer />
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Link } from 'gatsby'
 
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 const IndexPage = () => {
 
     return (
         <div>
+            <Header />
             <h1>Hello,</h1>
             <h2>I'm Bálint, a full-stack developer living in Hungary.</h2>
             <p>Need a developer? <Link to="/contact">Contact me</Link>.</p>
+            <Footer />
         </div>
     )
 }
