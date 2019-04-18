cd6344b90b527322ad68943be1d6c30e12f1a4c8
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-18 11:51:28 +0200

    Add about and contact page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I currently work as a full-stack developer.</p>
+        </div>
+    )
+}
+
+export default AboutPage
