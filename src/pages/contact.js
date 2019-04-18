b1c93fc9d8f6f3c13f01a4b254b9a06c7ac9f481
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-18 12:01:53 +0200

    Add links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ const ContactPage = () => {
     return (
         <div>
             <h1>Contact</h1>
-            <p>The best way to reach me is via Facebook.</p>
+            <p>The best way to reach me is via <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>.</p>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
 
@@ -6,6 +7,7 @@ const IndexPage = () => {
         <div>
             <h1>Hello,</h1>
             <h2>I'm Bálint, a full-stack developer living in Hungary.</h2>
+            <p>Need a developer? <Link to="/contact">Contact me</Link>.</p>
         </div>
     )
 }
