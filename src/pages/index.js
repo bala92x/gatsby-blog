69e5ef4eddcbb4456523da4485c00d7ee66841a0
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-18 11:42:35 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const IndexPage = () => {
+
+    return (
+        <div>
+            <h1>Hello,</h1>
+            <h2>I'm Bálint, a full-stack developer living in Hungary.</h2>
+        </div>
+    )
+}
+
+export default IndexPage
