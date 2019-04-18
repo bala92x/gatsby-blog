9a96ba95aa37c92eb465a80582b56e5228baee8b
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-18 12:46:11 +0200

    Add layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const ContactPage = () => {
     return (
         <div>
-            <Header />
-            <h1>Contact</h1>
-            <p>The best way to reach me is via <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>.</p>
-            <Footer />
+            <Layout>
+                <h1>Contact</h1>
+                <p>The best way to reach me is via <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>.</p>
+            </Layout>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 import React from "react"
 import { Link } from 'gatsby'
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
 
     return (
         <div>
-            <Header />
-            <h1>Hello,</h1>
-            <h2>I'm Bálint, a full-stack developer living in Hungary.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me</Link>.</p>
-            <Footer />
+            <Layout>
+                <h1>Hello,</h1>
+                <h2>I'm Bálint, a full-stack developer living in Hungary.</h2>
+                <p>Need a developer? <Link to="/contact">Contact me</Link>.</p>
+            </Layout>
         </div>
     )
 }
